f168fd54f2034d6715a57807b5ea81f54b252126
sonhai32 <lamsonhai1379@gmail.com>
2020-06-17 11:20:16 +0700

    add order user control

--- KetThucMon/Website/GioHang.Master.designer.cs
+++ KetThucMon/Website/GioHang.Master.designer.cs
@@ -38,5 +38,32 @@ namespace KetThucMon.Website {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Header1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::KetThucMon.TheHien.Header Header1;
+        
+        /// <summary>
+        /// GioHang1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::KetThucMon.TheHien.GioHang GioHang1;
+        
+        /// <summary>
+        /// Footer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::KetThucMon.TheHien.Footer Footer1;
     }
 }
